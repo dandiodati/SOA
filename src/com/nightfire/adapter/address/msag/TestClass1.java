12898d4820469a88b6d5720fb57dc8d4eb7514a4
harishgupta111 <harishgupta111@gmail.com>
2013-05-19 18:18:23 +0530

    Changes for Github Commit testing

--- src/com/nightfire/adapter/address/msag/TestClass1.java
+++ src/com/nightfire/adapter/address/msag/TestClass1.java
@@ -2,7 +2,7 @@ package com.nightfire.adapter.address.msag;
 
 public class TestClass1{
  void test1(){
-	 System.out.println("This is testing verson ");
+	 System.out.println("Changes in first version on github");
 	 
 	 
 	 System.out.println("Testing of sonar on 13th may");
